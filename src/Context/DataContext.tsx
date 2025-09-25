2b1a98b9572dd2efcb0249b1486221b71d693962
João <gustavociscotto@gmail.com>
2025-09-25 11:59:38 -0300

    feat ; saving in the localStorage

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -1,48 +1,77 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import type { OrderProps } from "../components/Sidebar/Sidebar";
 
 interface DataProvider {
-    children : React.ReactNode
+  children: React.ReactNode;
 }
 
 interface CardItem {
-    src : string
-    title : string,
-    description : string,
-    price : number,
+  src: string;
+  title: string;
+  description: string;
+  price: number;
 }
 
 interface DataContextProps {
-    Items : CardItem[] | undefined
-    setItems : React.Dispatch<React.SetStateAction<CardItem[]>>
-    addToList : (item : CardItem) => void
+  Items: CardItem[] | undefined;
+  setItems: React.Dispatch<React.SetStateAction<CardItem[]>>;
+  addToList: (item: CardItem) => void;
 
-    orderData : OrderProps | undefined
-    setOrderData : (data : OrderProps) => void
+  orderData: OrderProps | undefined;
+  setOrderData: (data: OrderProps) => void;
 }
 
-export const DataContext = createContext<DataContextProps>({} as DataContextProps)
+export const DataContext = createContext<DataContextProps>(
+  {} as DataContextProps
+);
 
+export function DataContextProvider({ children }: DataProvider) {
+  const [Items, setItems] = useState<CardItem[]>(() => {
+    const saveItem = localStorage.getItem("@Coffee-Delivery: Pedido")
 
-export function DataContextProvider({children} : DataProvider) {
-
-
-    const [Items, setItems] = useState<CardItem[]>([])
-
-    const [orderData, setOrderData] = useState<OrderProps>()
-
-    function addToList(item : CardItem) {
-        setItems((prevItems) => {
-            return [...prevItems, item]
-        })
+    if (saveItem) {
+        return JSON.parse(saveItem)
     }
 
+    return []
     
+  });
+
+  const [orderData, setOrderData] = useState<OrderProps | undefined >(() => {
+    const saveOrder = localStorage.getItem("@Coffee-Delivery: Endereço da entrega")
+
+    if (saveOrder && saveOrder !== "undefined") {
+        return JSON.parse(saveOrder)
+    }
+
+    return undefined
+    
+  });
+
+  useEffect(() => {
+    const ItemsJSON = JSON.stringify(Items);
+    const OrderJSON = JSON.stringify(orderData);
+
+    localStorage.setItem("@Coffee-Delivery: Pedido", ItemsJSON);
+    localStorage.setItem("@Coffee-Delivery: Endereço da entrega", OrderJSON);
+  }, [Items, orderData]);
 
 
-    return (
-        <DataContext.Provider value={{Items,setItems, addToList, orderData, setOrderData}}>
-            {children}
-        </DataContext.Provider>
-    )
+
+
+  
+
+  function addToList(item: CardItem) {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+  }
+
+  return (
+    <DataContext.Provider
+      value={{ Items, setItems, addToList, orderData, setOrderData }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
 }
--- src/components/CardContainerPay/CardContainerPay.tsx
+++ src/components/CardContainerPay/CardContainerPay.tsx
@@ -1,82 +1,88 @@
-
-import { useContext } from "react"
-import { CardCoffeePay } from "../CardCoffeePay/CardCoffeePay"
-import styles from "./styles.module.css"
-import { DataContext } from "../../Context/DataContext"
-
+import { useContext } from "react";
+import { CardCoffeePay } from "../CardCoffeePay/CardCoffeePay";
+import styles from "./styles.module.css";
+import { DataContext } from "../../Context/DataContext";
 
 interface CardContainerPayProps {
-    onConfirm : () => void
+  onConfirm: () => void;
 }
 
-export function CardContainerPay({onConfirm} : CardContainerPayProps) {
-    
-    
+export function CardContainerPay({ onConfirm }: CardContainerPayProps) {
+  const { Items } = useContext(DataContext);
 
-    const {Items} = useContext(DataContext)
-    
-    console.log(Items)
+  console.log(Items);
 
-    
-   const sumTotal = Items?.reduce((acc, current) => {
-            return acc + current.price
-   }, 0)
+  const sumTotal = Items?.reduce((acc, current) => {
+    return acc + current.price;
+  }, 0);
 
+  const deliveryPrice = 3.5;
 
-   const deliveryPrice = 3.50
+  const total = (sumTotal ?? 0) + deliveryPrice;
 
-   
-   
-   const total = (sumTotal ?? 0) + deliveryPrice
-    
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Cafés selecionados</h2>
 
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>Cafés selecionados</h2>
+      <div className={styles.cardContainer}>
+        {Items?.map((item) => (
+          <CardCoffeePay
+            key={item.title}
+            src={item.src}
+            title={item.title}
+            price={item.price}
+          />
+        ))}
 
-            <div className={styles.cardContainer}>
+        <div className={styles.totalContainer}>
+          <div>
+            <span>Total de itens</span>{" "}
+            {sumTotal ? (
+              <span>
+                {sumTotal.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            ) : (
+              <span>0</span>
+            )}
+          </div>
+          <div>
+            <span>Entrega</span>
+            {sumTotal ? (
+              <span>
+                {deliveryPrice.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            ) : (
+              <span>0</span>
+            )}
+          </div>
 
-                {Items?.map((item) => <CardCoffeePay key={item.title} src = {item.src} title = {item.title} price = {item.price}  />)}
-
-               
-
-                
-                
-
-
-
-            <div className={styles.totalContainer}>
-
-            <div>
-                <span>Total de itens</span> {sumTotal ? <span>{sumTotal.toLocaleString("pt-BR", {
-                    style : "currency",
-                    currency : "BRL"
-                })}</span> : <span>0</span>}
-            </div>
-            <div>
-                <span>Entrega</span> <span>{deliveryPrice.toLocaleString("pt-BR", {
-                    style : "currency",
-                    currency : "BRL"
-                })}</span>
-            </div>
-            <div>
-                <span>Total</span>  <span>{total.toLocaleString("pt-BR", {
-                    style : "currency",
-                    currency : "BRL"
-                })}</span>
-            </div>
-
+          <div>
+            <span>Total</span>{" "}
+            <span>
+             {sumTotal ? (
+              <span>
+                {total.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            ) : (
+              <span>0</span>
+            )}
+            </span>
+          </div>
         </div>
 
         <button onClick={onConfirm} className={styles.buttonConfirmed}>
-            CONFIRMAR PEDIDO
+          CONFIRMAR PEDIDO
         </button>
-                
-                
-            </div>
-
-            
-                
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
